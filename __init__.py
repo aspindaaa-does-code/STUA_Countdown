0823a7a32f718460d1794b7dca2fe186c7e026c5
aspindaaa-does-code <ravindramangar510@gmail.com>
2023-12-21 18:42:50 -0500

    Added additional comments to __init__.py

--- __init__.py
+++ __init__.py
@@ -10,15 +10,22 @@ To properly terminate this program, type anything into the console and press ent
 ----------------------------------------
 """
 
+# Imports
 import subprocess, multiprocessing
 
+
 if __name__ in "__main__":
-    multiprocessing.freeze_support()
+    multiprocessing.freeze_support() # Enables freezing of the multiprocessing module, to allow for multiprocessing to work on Windows.
+    
+    # Runs the STUA Countdown Board API, web app, and main program.
     main_api = subprocess.Popen(["python3", "main_api.py"])
     main_proc = subprocess.Popen(["python3", "main_proc.py"])
     main_web = subprocess.Popen(["python3", "main_web.py"])
+
+    # Allows for the program to be terminated by typing anything into the console and pressing enter.
     while True:
         if input():
+            # Terminates the API, web app, and main program.
             main_api.kill()
             main_proc.kill()
             main_web.kill()
